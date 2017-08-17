e993c5230f94e90bc7278c2de2eb4d7ca533acce
Jonathan Hurter <john@scalingo.com>
2017-08-17 23:13:28 +0200

    Fix: User script are runned as root too
    Signed-off-by: Jonathan Hurter <john@scalingo.com>

--- custom/installer.sh
+++ custom/installer.sh
@@ -5,26 +5,28 @@ export PROJECT_ROOT="$(dirname "$0")/../"
 source $PROJECT_ROOT/libraries/bootstrap.sh
 load $PROJECT_ROOT/libraries
 
-info "Starting custom synchronisation"
+if [ -z $1 ] ; then
+  info "Starting custom synchronisation"
 
-for file in $(ls $PROJECT_ROOT/custom/user_scripts) ; do
-  log "Running $file"
-  bash $PROJECT_ROOT/custom/wrapper.sh $PROJECT_ROOT $PROJECT_ROOT/custom/user_scripts/$file
-  failFast $? "Failed to run $file custom script"
-done
+  for file in $(ls $PROJECT_ROOT/custom/user_scripts) ; do
+    log "Running $file"
+    bash $PROJECT_ROOT/custom/wrapper.sh $PROJECT_ROOT $PROJECT_ROOT/custom/user_scripts/$file
+    failFast $? "Failed to run $file custom script"
+  done
 
-success "Continuing as root"
+  success "Continuing as root"
 
-if [ ! `whoami` = root ] ; then
-  info "Switching to root"
-  exec sudo bash $0
+  if [ ! `whoami` = root ] ; then
+    info "Switching to root"
+    exec sudo bash $0 root
+  fi
+else
+  for file in $(ls $PROJECT_ROOT/custom/root_scripts) ; do
+    log "Running $file"
+    bash $PROJECT_ROOT/custom/wrapper.sh $PROJECT_ROOT $PROJECT_ROOT/custom/root_scripts/$file
+    failFast $? "Failed to run $file custom script"
+  done
+
+  success "Custom scripts runned"
 fi
-
-for file in $(ls $PROJECT_ROOT/custom/root_scripts) ; do
-  log "Running $file"
-  bash $PROJECT_ROOT/custom/wrapper.sh $PROJECT_ROOT $PROJECT_ROOT/custom/root_scripts/$file
-  failFast $? "Failed to run $file custom script"
-done
-
-success "Custom scripts runned"
 
