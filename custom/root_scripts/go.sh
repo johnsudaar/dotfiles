850036617843f2f9de34a5c40a1e0332e0074a54
Jonathan Hurter <john@scalingo.com>
2017-08-17 23:49:22 +0200

    Add go
    Signed-off-by: Jonathan Hurter <john@scalingo.com>

---
+++ custom/root_scripts/go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version=1.8.3
+
+/usr/local/go/bin/go version | grep $version > /dev/null
+
+if [ $? -eq 0 ] ; then
+  log "Go installed. Skipping..."
+  exit 0
+fi
+
+log "Downloading go $version"
+curl -s -O https://storage.googleapis.com/golang/go$version.linux-amd64.tar.gz
+failFast $? "Fail to download go"
+
+log "Installing go $version"
+tar -C /usr/local -xzf go$version.linux-amd64.tar.gz
+failFast $? "Fail to extract go"
+
+rm go$version.linux-amd64.tar.gz
+
+success "Go $version installed."
+
