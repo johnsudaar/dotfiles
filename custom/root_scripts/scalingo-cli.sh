3d2df60105fe6eef176ca19e41525f730c6b1055
Jonathan Hurter <john@scalingo.com>
2017-08-22 17:47:03 +0200

    Add scalingo cli update management

--- custom/root_scripts/scalingo-cli.sh
+++ custom/root_scripts/scalingo-cli.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 if [ -f /usr/local/bin/scalingo ] ; then
-  log "Scalingo cli already installed. Skipping..."
-  exit 0
+  scalingo version | grep "Please update"
+
+  if [ $? -ne 0 ] ; then
+    log "Scalingo cli already installed. Skipping..."
+    exit 0
+  else
+    log "New version available. Installing ..."
+  fi
 fi
 
-curl -sO https://cli-dl.scalingo.io/install && bash install
+curl -sO https://cli-dl.scalingo.io/install && yes | bash install
 failFast $? "Fail to install scalingo cli"
+
+success "Scalingo CLI installed"
