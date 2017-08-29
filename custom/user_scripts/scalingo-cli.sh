885f70e4e5e078afe4d869e61fcd24ce3e202996
Jonathan Hurter <john@scalingo.com>
2017-08-29 12:26:11 +0200

    Add scalingo autocompletion

---
+++ custom/user_scripts/scalingo-cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -f $HOME/.zsh/completion/scalingo_complete.zsqh ] ; then
+  log "Scalingo cli completion installed. Skipping..."
+  exit 0
+fi
+
+mkdir -p $HOME/.zsh/completion/
+curl "https://raw.githubusercontent.com/Scalingo/cli/master/cmd/autocomplete/scripts/scalingo_complete.zsh" > ~/.zsh/completion/scalingo_complete.zsh
+failFast $? "Fail to install scalingo autocompletion"
+
+success "Scalingo cli autocompletion installed"
+
