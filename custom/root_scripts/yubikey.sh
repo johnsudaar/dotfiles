75bbe6ac4d90070bec121581830bc9e153493f44
Jonathan Hurter <john@scalingo.com>
2017-11-28 16:52:05 +0100

    Add yubikey configuration

---
+++ custom/root_scripts/yubikey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+systemctl enable pcscd
+systemctl start pcscd
+
+installFile "$PROJECT_ROOT/custom/config/system-auth" "/etc/pam.d/system-auth"
+installStatus=$?
+if [ $installStatus -ne 1 ] ; then
+  failFast $installStatus "Fail to install system auth config"
+fi
+
+installFile "$PROJECT_ROOT/custom/config/yubikeys" "/etc/yubikeys"
+installStatus=$?
+if [ $installStatus -ne 1 ] ; then
+  failFast $installStatus "Fail to install yubikeys"
+fi
+success "Yubikey configured"
