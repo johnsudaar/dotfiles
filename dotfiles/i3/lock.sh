489696bfff593e4846a2f915b46b4b8778d84870
Jonathan Hurter <john@scalingo.com>
2018-01-04 02:25:38 +0100

    Bit quicker blur

--- dotfiles/i3/lock.sh
+++ dotfiles/i3/lock.sh
@@ -23,13 +23,13 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
 #BLURTYPE="0x5" # 7.52s
-#BLURTYPE="0x2" # 4.39s
-BLURTYPE="5x2" # 3.80s
+BLURTYPE="0x3" # 4.39s
+#BLURTYPE="5x2" # 3.80s
 #BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
-convert $IMAGE -blur $BLURTYPE $IMAGE
+convert $IMAGE -resize 50% -blur $BLURTYPE -resize 200% $IMAGE
 i3lock -i $IMAGE
 rm $IMAGE
